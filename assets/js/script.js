1266eb6d144835ac7374c5d53710d74c6ee909f0
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-11-13 09:28:04 -0400

    updat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 var highScore = document.querySelector(".highScore");
-var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 
-var quizBoxQ = document.querySelector(".quiz-area");
+var quizQuest = document.querySelector(".quiz-area");
 
 var timerElement = document.querySelector(".timer-count");
 var timerCount;
@@ -16,22 +15,29 @@ const multipleChoice = {
     stat: ["Single","Complicated","Seperated","Married"]
 }
 var chosenQuestion="";
-
-var listEl = document.createElement("ol");
+var listBtn = document.createElement("ol")
 var but1 = document.createElement("button")
 var but2 = document.createElement("button")
 var but3 = document.createElement("button")
 var but4 = document.createElement("button")
 
+function btnFormat() {
+  listBtn.setAttribute("style","width:100px; display:block; padding: 10px;text-align: left")  
+}
+
+function btnGroup() {  
+  quizQuest.appendChild(listBtn);
+  listBtn.appendChild(but1).textContent="1. "+multipleChoice["name"][0];
+  listBtn.appendChild(but2).textContent="2. "+multipleChoice["name"][1];
+  listBtn.appendChild(but3).textContent="3. "+multipleChoice["name"][2];
+  listBtn.appendChild(but4).textContent="4. "+multipleChoice["name"][3];
+}
 
 function showQuizs() {
   chosenQuestion = questions[0];
-  quizBoxQ.textContent = chosenQuestion;
-  quizBoxQ.appendChild(listEl);
-  listEl.appendChild(but1).textContent="1. "+multipleChoice["name"][0];
-  listEl.appendChild(but2).textContent="2. "+multipleChoice["name"][1];
-  listEl.appendChild(but3).textContent="3. "+multipleChoice["name"][2];
-  listEl.appendChild(but4).textContent="4. "+multipleChoice["name"][3];
+  quizQuest.textContent = chosenQuestion;
+  btnGroup()
+  btnFormat()
 }
 
 function startTimer() {
@@ -58,7 +64,7 @@ function startTimer() {
   }
 
 function startGame(){
-    timerCount = 50;
+    timerCount = 60;
     startTimer()
     showQuizs()
 }

0e07841fa603022cfc926717f3e56d09710431d3
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-11-13 02:27:59 -0400

    saved files

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,29 @@ var timer;
 
 var questions = ["What is your name?","How old are you?","What is your status?"]
 var answers = ["Yel","45","Married"]
-var multipleChoice = {
+const multipleChoice = {
     name: ["Bob","Mike","Yel","John"],
     age: ["40","43","45","48"],
     stat: ["Single","Complicated","Seperated","Married"]
 }
+var chosenQuestion="";
 
+var listEl = document.createElement("ol");
+var but1 = document.createElement("button")
+var but2 = document.createElement("button")
+var but3 = document.createElement("button")
+var but4 = document.createElement("button")
+
+
+function showQuizs() {
+  chosenQuestion = questions[0];
+  quizBoxQ.textContent = chosenQuestion;
+  quizBoxQ.appendChild(listEl);
+  listEl.appendChild(but1).textContent="1. "+multipleChoice["name"][0];
+  listEl.appendChild(but2).textContent="2. "+multipleChoice["name"][1];
+  listEl.appendChild(but3).textContent="3. "+multipleChoice["name"][2];
+  listEl.appendChild(but4).textContent="4. "+multipleChoice["name"][3];
+}
 
 function startTimer() {
     // Sets timer
@@ -32,16 +49,17 @@ function startTimer() {
         }
       
       // Tests if time has run out
-    //   if (timerCount === 0) {
-    //     // Clears interval
-    //     clearInterval(timer);
-    //     loseGame();
-    //   }
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        // loseGame();
+      }
     }, 1000);
   }
 
 function startGame(){
-    timerCount = 60;
-    startTimer()   
+    timerCount = 50;
+    startTimer()
+    showQuizs()
 }
 startButton.addEventListener("click", startGame);

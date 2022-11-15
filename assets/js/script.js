ff6e0118f26affb4c68a00243942f9ca7655c299
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-11-14 21:20:01 -0400

    some update in code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var highScore = document.querySelector(".highScore");
 var startButton = document.querySelector(".start-button");
+var header=document.querySelector("header");
 
-var quizQuest = document.querySelector(".quiz-area");
-var statmentOfanswer = document.querySelector(".statement");
+var initalState = document.getElementById("initial-state"); 
+var quizQuest = document.querySelector(".quiz-state")//
+var statmentOfanswer = document.querySelector(".statement");//
 
 var timerElement = document.querySelector(".timer-count");
 var timerCount;
@@ -23,18 +25,19 @@ var correctAnswer;
 var quesTion= 0;
 var questionLength = 0;
 
+var winCount=0;
+
 var listBtn = document.createElement("label")
 var but1 = document.createElement("button")
 var but2 = document.createElement("button")
 var but3 = document.createElement("button")
 var but4 = document.createElement("button")
-but1.id="but1";
-but2.id="but2";
-but3.id="but3";
-but4.id="but4";
 
 function btnFormat() {
-  listBtn.setAttribute("style","width:100px; display:block; padding: 10px;text-align: left")  
+  but1.setAttribute("style","width:auto; margin:10px; display:block; text-align: left");
+  but2.setAttribute("style","width:auto; margin:10px; display:block; text-align: left");
+  but3.setAttribute("style","width:auto; margin:10px; display:block; text-align: left");
+  but4.setAttribute("style","width:auto; margin:10px; display:block; text-align: left"); 
 }
 
 function btnGroup0(list) {  
@@ -45,7 +48,7 @@ function btnGroup0(list) {
   listBtn.appendChild(but4).textContent="4. "+multipleChoice[list][3];  
 }
 
-function showQuizs0() {
+function showQuizs() {
   btnFormat()
   if (questionLength<=2){
     var j=questionLength;
@@ -54,11 +57,9 @@ function showQuizs0() {
     quizQuest.textContent = chosenQuestion;
     quesTion = typeOfquest[j];    
   }
-  else {
-    return
-  }
-  btnGroup0(quesTion) 
+  btnGroup0(quesTion)
 }
+
 
 function startTimer() {
     // Sets timer
@@ -66,20 +67,37 @@ function startTimer() {
       timerCount--;
       timerElement.textContent = timerCount;
       if (timerCount >= 0) {
-        // console.log(timerCount);
         // Tests if win condition is met
         if (isWin && timerCount > 0) {
           // Clears interval and stops timer
+          winCount++;
+          console.log(winCount);
         }
       }
       // Tests if time has run out
+      
       if (timerCount === 0 || questionLength==3) {
         // Clears interval
         clearInterval(timer);
-        // loseGame();
-        quizQuest.textContent="You made it.";
+        // EndGame;
+        if (timerCount ===0){
+          quizQuest.textContent="Sorry, Time up!";
+        }
+        else {
+          quizQuest.textContent="You answered all questions. Awesome!";
+        }
+        
+        header.setAttribute("style","display:none")
         statmentOfanswer.setAttribute("style","display:none")
 
+        var timerCount2 = 10;
+        var timer2 = setInterval(function(){
+            timerCount2--;
+            if (timerCount2 === 0) {
+            clearInterval(timer2);
+            checkScore()
+            }
+        },100);
       }
     }, 1000);
   }
@@ -87,58 +105,75 @@ function startTimer() {
 function startGame(){
     timerCount = 60;
     startTimer()
-    showQuizs0()
+    showQuizs()    
 }
 
+function checkScore(){
+  quizQuest.textContent="Your score is " + winCount+".";
+  localStorage.setItem("winCounter", winCount);
+}
+
+function recordHigh() {
+  
+}
 function checkAnswer(i){
-  var isCorrect=false;
+    
   if(multipleChoice[quesTion][i]===correctAnswer){
     console.log("You are correct.");
     statmentOfanswer.textContent="Correct";
     statmentOfanswer.setAttribute("style","display:flex")
     isCorrect=true;
+    winCount = winCount+10;
   }
   else {
     console.log("Nice try, incorrect.");
     statmentOfanswer.textContent="Nice try, incorrect";
     statmentOfanswer.setAttribute("style","display:flex")
+    timerCount = timerCount-5;
+    timerElement.textContent = timerCount;    
   };
+  console.log(winCount);
   questionLength++;
-  showQuizs0()
-  // if (questionLengt<=3) {
-  //   
-  //   showQuizs0()
-  // }
-  // else {
-  // quizQuest.textContent="You completed.";
-  // }
+
+  var timerCount2 = 3;
+  var timer2 = setInterval(function(){
+   timerCount2--;
+   if (timerCount2 === 0) {
+    clearInterval(timer2);
+    showQuizs()
+   }
+  },100);
+  
 }
 
 function but1Button(){
   choseButton = 0;
   console.log(correctAnswer);
   console.log(quesTion);
-  checkAnswer(choseButton)
+  checkAnswer(choseButton);
 }
 function but2Button(){
   choseButton = 1;
   console.log(correctAnswer);
   console.log(quesTion);
-  checkAnswer(choseButton)
+  checkAnswer(choseButton);
 }
 function but3Button(){
   choseButton = 2;
   console.log(correctAnswer);
   console.log(quesTion);
-  checkAnswer(choseButton)
+  checkAnswer(choseButton);
 }
 function but4Button(){
   choseButton = 3;
   console.log(correctAnswer);
   console.log(quesTion);
-  checkAnswer(choseButton)
+  checkAnswer(choseButton);
 }
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", function(){
+  initalState.setAttribute("style","display: none");
+  startGame()
+})
 but1.addEventListener("click",but1Button);
 but2.addEventListener("click",but2Button);
 but3.addEventListener("click",but3Button);
